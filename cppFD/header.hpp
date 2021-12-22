a407c7e708cc8539269b46ee8abb107bd2173cb0
Duke Nguyen <duke7295@gmail.com>
2021-12-22 08:45:57 +0100

    Initialize

---
+++ cppFD/header.hpp
@@ -0,0 +1,19 @@
+//standard lib folder, cout, cin, getline
+#include <iostream>
+#include <vector>
+#include <fstream>
+// formating output
+#include <iomanip>
+#include <time.h> 
+#include <stdio.h>
+#include <string>
+#include <sstream>
+#include <cmath>
+
+// using namespace std;
+
+// Functions
+// declaration before main
+// describe the function (purposes, inputs, outputs)
+// TODO
+retVariableType functionName(paramsVariableType params1);
